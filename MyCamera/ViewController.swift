40af8421a0444f9c66b4b4019221ef3f01264bb7
yumi KB <bon.kyumi@gmail.com>
2020-11-07 00:02:54 +0900

    Open camera and set Images

--- MyCamera/ViewController.swift
+++ MyCamera/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var pictureImage: UIImageView!
+    
+    @IBAction func cameraButtonAction(_ sender: Any) {
+        // カメラが利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("カメラは利用できます")
+            
+            // UIImagePickerControllerのインスタンスを作成
+            let imagePickerController = UIImagePickerController()
+            
+            // sourceTypeにcameraを指定
+            imagePickerController.sourceType = .camera
+            
+            // delegate設置
+            imagePickerController.delegate = self
+            
+            // モーダルビューで表示
+            present(imagePickerController, animated: true, completion: nil)
+        } else {
+            print("カメラは利用できません")
+        }
+    }
+    
+    @IBAction func SNSButtonAction(_ sender: Any) {
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        // 撮影した写真を、配置したpictureImageに渡す
+        pictureImage.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        
+        // モーダルビューを閉じる
+        dismiss(animated: true, completion: nil)
+    }
 }
 
